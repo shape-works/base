78a39e4bf8b416c457a5c384f84dbce3329cdb3f
marc-andre-purr <marc-andre.deschamps@purrgroup.com>
2023-01-25 12:54:48 +0000

    Update register_block_types.php

--- backend/register_block_types.php
+++ backend/register_block_types.php
@@ -26,14 +26,14 @@ add_action( 'init', function() {
 			true
 		);
 
-		//Default: Any Post Type
-		$allowed_block_types['all'][] = $block_json['name'];
-
+	
 		if( isset($block_json['postTypes']) ) {//Specific Post Type
 			foreach( $block_json['postTypes'] as $post_type ) {
 				$allowed_block_types[$post_type][] = $block_json['name'];
 			}
-		} 
+		} else {//Default: Any Post Type
+			$allowed_block_types['all'] = $block_json['name'];
+		}
 
 		$block_folder_path = str_replace('/block.json', '', $block_json_path);
 		
