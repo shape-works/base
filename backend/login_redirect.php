c07e0a1f9e68f4a8085bb2f0529705c88868f885
marc-andre-purr <marc-andre.deschamps@purrgroup.com>
2023-03-03 15:12:27 +0000

    update

--- backend/login_redirect.php
+++ backend/login_redirect.php
@@ -4,6 +4,7 @@ add_filter( 'login_redirect', function(){
 	return admin_url().'/edit.php?post_type=page';
 }, 10, 3 );
 
+
 //Redirect to Page when reaching dashboard
 add_action('load-index.php',function(){
 	wp_redirect(admin_url('edit.php?post_type=page'));
