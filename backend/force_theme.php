6b7e3201c83f6d5f7601e40ebc47e69b88d78885
marc-andre-purr <marc-andre.deschamps@purrgroup.com>
2024-10-10 12:48:24 +0100

    update fallback for older projects

--- backend/force_theme.php
+++ backend/force_theme.php
@@ -2,9 +2,16 @@
 defined('ABSPATH') or die("No direct access");
 
 /**
- * Ensure our theme is always active
+ * Ensure our theme is always active with a fallback to 'paws' if 'theme' does not exist.
  */
-
 add_action('setup_theme', function () {
-    switch_theme('theme');
+    $primary_theme = 'theme';
+    $fallback_theme = 'paws';
+
+    // Check if the primary theme exists
+    if (wp_get_theme($primary_theme)->exists()) {
+        switch_theme($primary_theme); // Activate primary theme
+    } else {
+        switch_theme($fallback_theme); // Fallback to 'paws' theme if 'theme' doesn't exist
+    }
 });
