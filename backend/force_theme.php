060d545a023f63d8c263e74de2de9c1d3f703532
marc-andre-purr <marc-andre.deschamps@purrgroup.com>
2024-12-18 10:20:44 +0000

    Update Force Theme fn (hotfix)

--- backend/force_theme.php
+++ backend/force_theme.php
@@ -4,14 +4,19 @@ defined('ABSPATH') or die("No direct access");
 /**
  * Ensure our theme is always active with a fallback to 'paws' if 'theme' does not exist.
  */
-add_action('setup_theme', function () {
+add_action('init', function () {
     $primary_theme = 'theme';
     $fallback_theme = 'paws';
 
-    // Check if the primary theme exists
-    if (wp_get_theme($primary_theme)->exists()) {
-        switch_theme($primary_theme); // Activate primary theme
-    } else {
-        switch_theme($fallback_theme); // Fallback to 'paws' theme if 'theme' doesn't exist
+    // Check the current active theme
+    $active_theme = wp_get_theme()->get_stylesheet();
+
+    if ($active_theme !== $primary_theme) {
+        // Check if the primary theme exists
+        if (wp_get_theme($primary_theme)->exists()) {
+            switch_theme($primary_theme); // Activate primary theme
+        } elseif (wp_get_theme($fallback_theme)->exists()) {
+            switch_theme($fallback_theme); // Fallback to 'paws' theme if 'theme' doesn't exist
+        }
     }
 });
